277a3c5f89c619f8ddf84bcf18f0e391d7b7ee0e
russa <aaron.russ@dfki.de>
2020-09-18 22:08:32 +0200

    format: reformatted speexEncoder.js

--- www/webworker/speexEncoder.js
+++ www/webworker/speexEncoder.js
@@ -31,16 +31,16 @@ if(typeof WEBPACK_BUILD !== 'undefined' && WEBPACK_BUILD){
 
 function TheSpeexEncoder(){
 
-    var libspeex = self.libspeex;
+	var libspeex = self.libspeex;
 
-    var bits = libspeex._malloc(Speex.types.SpeexBits.__size__);
-    libspeex._speex_bits_init(bits);
+	var bits = libspeex._malloc(Speex.types.SpeexBits.__size__);
+	libspeex._speex_bits_init(bits);
 
-    var encoder = libspeex._speex_encoder_init(libspeex._speex_lib_get_mode(1));
-    var buffer_ptr = libspeex._malloc(320*2);
-    var buffer = libspeex.HEAP16.subarray(buffer_ptr/2, buffer_ptr/2+320);
-    var out_buffer_ptr = libspeex._malloc(100);
-    var out_buffer = libspeex.HEAPU8.subarray(out_buffer_ptr, out_buffer_ptr+100);
+	var encoder = libspeex._speex_encoder_init(libspeex._speex_lib_get_mode(1));
+	var buffer_ptr = libspeex._malloc(320*2);
+	var buffer = libspeex.HEAP16.subarray(buffer_ptr/2, buffer_ptr/2+320);
+	var out_buffer_ptr = libspeex._malloc(100);
+	var out_buffer = libspeex.HEAPU8.subarray(out_buffer_ptr, out_buffer_ptr+100);
 
 	//target samplerate for encoding speex wb:
 	var desiredSampleRate = 16000;
@@ -69,38 +69,37 @@ function TheSpeexEncoder(){
 		return self.doResample(buffer)
 	};
 
-	 this.encodeBuffer = function(buff){
+	this.encodeBuffer = function(buff){
 
-		    //resample
-		    var frame = this.resample(buff);
+		//resample
+		var frame = this.resample(buff);
 
-		    var offset = 0;
+		var offset = 0;
 
-            var ret = [];
-            var frame_offset = 0;
-            while(frame_offset < frame.length){
-                var size = Math.min(320-offset, frame.length-frame_offset);
-                for(var i=0; i<size; i++){
-                    buffer[offset+i] = frame[frame_offset+i] * 32767.0;
-                }
-                frame_offset += size;
-                offset += size;
-                if(offset<320){
-                    break;
-                }
-                offset = 0;
-                var status = libspeex._speex_encode_int(encoder,buffer_ptr,bits);
-                var count = libspeex._speex_bits_nbytes(bits);
-                status = libspeex._speex_bits_write(bits,out_buffer_ptr,count);
-                status = libspeex._speex_bits_reset(bits);
-                var out_frame = new Uint8Array(count);
-                out_frame.set(out_buffer.subarray(0,count));
-                ret.push(out_frame);
-            }
+		var ret = [];
+		var frame_offset = 0;
+		while(frame_offset < frame.length){
+			var size = Math.min(320-offset, frame.length-frame_offset);
+			for(var i=0; i<size; i++){
+					buffer[offset+i] = frame[frame_offset+i] * 32767.0;
+			}
+			frame_offset += size;
+			offset += size;
+			if(offset<320){
+					break;
+			}
+			offset = 0;
+			var status = libspeex._speex_encode_int(encoder,buffer_ptr,bits);
+			var count = libspeex._speex_bits_nbytes(bits);
+			status = libspeex._speex_bits_write(bits,out_buffer_ptr,count);
+			status = libspeex._speex_bits_reset(bits);
+			var out_frame = new Uint8Array(count);
+			out_frame.set(out_buffer.subarray(0,count));
+			ret.push(out_frame);
+		}
 
-		    this.encoded = ret;
-
-	    };
+		this.encoded = ret;
+	};
 }
 
 //export into global instance variable (see encoder.js for sending/receiving messages on this)
